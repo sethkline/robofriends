af0e144fa244aba98178d7489de0fcf6ccee4f17
Mac <user@Edit-5-1.local>
2018-08-16 16:53:58 -0400

    search working

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Cardlist from  './Cardlist';
 import SearchBox from './SearchBox';
 import { robots } from './robots';
 
-const App = () => {
+const state = {
+    robots: robots,
+    searchfield: ''
+}
+
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            robots: robots,
+            searchfield: ''
+        }
+    }
+
+    onSearchChange = (event) => {
+        this.setState({ searchfield: event.target.value })
+
+    }
+
+    render(){
+            const filteredRobots = this.state.robots.filter(robots => {
+            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        })
     return (
         <div className='tc'>
             <h1>RoboFriends</h1>
-            <SearchBox />
+            <SearchBox searchChange={this.onSearchChange}/>
 
-        <Cardlist robots={robots} />
+            <Cardlist robots={filteredRobots} />
 
         </div>
     );
 }
+}
 
-export default App
+export default App;
