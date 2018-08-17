05b2254680faed7476dde1cbd4fddfe3d5a2944e
Mac <user@Edit-5-1.local>
2018-08-17 16:18:01 -0400

    added info from faker api

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import React, { Component } from 'react';
 import Cardlist from  './Cardlist';
 import SearchBox from './SearchBox';
-import { robots } from './robots';
+import './app.css';
 
-const state = {
-    robots: robots,
-    searchfield: ''
-}
 
 class App extends Component {
     constructor() {
         super()
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
+    }
+
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response =>  response.json())
+        .then(users => this.setState({ robots : users}));
+        
     }
 
     onSearchChange = (event) => {
@@ -26,15 +29,19 @@ class App extends Component {
             const filteredRobots = this.state.robots.filter(robots => {
             return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
         })
-    return (
-        <div className='tc'>
-            <h1>RoboFriends</h1>
-            <SearchBox searchChange={this.onSearchChange}/>
+        if (this.state.robots.length === 0) {
+            return <h1>Loading</h1>
+        } else {
+            return (
+                <div className='tc'>
+                    <h1 className='f1'>RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange}/>
 
-            <Cardlist robots={filteredRobots} />
+                    <Cardlist robots={filteredRobots} />
 
-        </div>
+                </div>
     );
+}
 }
 }
 
